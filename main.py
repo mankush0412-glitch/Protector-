480c0c43457303a6d63c48dd72c0289c7ca3cbd9
mankush0412-glitch <mankush0412@gmail.com>
2026-03-21 05:31:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -158,9 +158,10 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     )
 
     # 🔐 FORCE JOIN — FOR ALL USERS (NORMAL + PROTECTED)
- if not await check_channel_membership(user_id, context):
-    callback_data = f"check_join_{context.args[0]}" if context.args else "check_join"
-    keyboard = []
+    
+    if not await check_channel_membership(user_id, context):
+     callback_data = f"check_join_{context.args[0]}" if context.args else "check_join"
+     keyboard = []
 
     for ch in get_support_channels():
         try:
